4b9a319982cabcc7bf73bf75a7f69a917422366e
sowmyagiri08 <sowmyagiri08@gmail.com>
2020-09-17 16:21:03 +0530

    Deleted sub repository

--- News-SPA1/src/app/article.ts
+++
@@ -1,7 +0,0 @@
-export class Article {
-
-    constructor(public articleId: string, public source: string, public author: string, public title: string, public description: string, public url: string,
-        public urlToImage: string, public publishedAt: string, public content: string, public articleAddedBy: string) {
-            
-    }
-}
